4584d31fdbd3b421f35e9430e25694ad63c29ca6
101Pazzi <whatslife.pazzi@gmail.com>
2022-05-28 12:31:34 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//import modules
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+//create environment variables
+const PORT = process.env.PORT || 8000;
+
+http.createServer((req, res) => {
+    
+    if (req.url === '/'){
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        });
+    }
+
+}).listen(PORT, () => console.log(`Server running on port:${PORT}`));
