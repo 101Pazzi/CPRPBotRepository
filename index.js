cd0a128ea466e84fc1568c9b19b7cae183ad8e84
101Pazzi <whatslife.pazzi@gmail.com>
2022-05-29 09:20:20 -0600

    add some tmi.js usage

--- index.js
+++ index.js
@@ -2,9 +2,24 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const tmi = require('tmi.js');
+const secrets = require('./secretStuff');
 
 //create environment variables
 const PORT = process.env.PORT || 8000;
+
+//create the tmi client
+const client = new tmi.Client({
+	options: { debug: false },
+	identity: {
+		username: 'CPRPBot',
+		password: secrets.password
+	},
+	channels: secrets.channels,
+    connection: {
+        port: PORT
+    }
+});
 
 http.createServer((req, res) => {
     
@@ -17,4 +32,16 @@ http.createServer((req, res) => {
     }
 
 }).listen(PORT, () => console.log(`Server running on port:${PORT}`));
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
 
